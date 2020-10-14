b5df1147cdb9918b8b34c3a06fc1ac849f5defc7
태재영 <tjy970721@gmail.com>
2020-10-14 16:26:08 +0900

    0.1.1

--- dist/Noliter.d.ts
+++ dist/Noliter.d.ts
@@ -3,9 +3,9 @@ declare class Noliter<K extends keyof HTMLElementTagNameMap> {
     dom: HTMLElementTagNameMap[K];
     constructor(tagName: K);
     append(...children: (string | Node | Noliter<K>)[]): this;
-    setAttributes(...attributes: Primitive[]): this;
-    setProperties(...properties: Primitive[]): this;
-    addEvent<E extends keyof HTMLElementEventMap>(type: E, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): this;
+    attrs(...attributes: Primitive[]): this;
+    props(...properties: Primitive[]): this;
+    events<E extends keyof HTMLElementEventMap>(type: E, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): this;
 }
 export default function $<K extends keyof HTMLElementTagNameMap>(tagName: K): Noliter<keyof HTMLElementTagNameMap>;
 export {};
