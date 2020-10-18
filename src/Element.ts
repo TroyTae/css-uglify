a33bb72d12882c9577f6272044599f4e05c955ff
태재영 <tjy970721@gmail.com>
2020-10-18 22:31:09 +0900

    feat: p

--- src/Element.ts
+++ src/Element.ts
@@ -64,3 +64,4 @@ export function createOl() { return new Noliter<HTMLOListElement>('ol'); }
 export function createOptGroup() { return new Noliter<HTMLOptGroupElement>('optgroup'); }
 export function createOption() { return new Noliter<HTMLOptionElement>('option'); }
 export function createOutput() { return new Noliter<HTMLOutputElement>('output'); }
+export function createParagraph() { return new Noliter<HTMLParagraphElement>('p'); }
