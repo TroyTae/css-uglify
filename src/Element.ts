886fd9436f24ad28a45dd90f2d893786fc7e61dc
태재영 <tjy970721@gmail.com>
2020-10-18 20:24:53 +0900

    feat: bold

--- src/Element.ts
+++ src/Element.ts
@@ -7,3 +7,4 @@ export function createArea() { return new Noliter<HTMLAreaElement>('area'); }
 export function createArticle() { return new Noliter('article'); }
 export function createAside() { return new Noliter('aside'); }
 export function createAudio() { return new Noliter<HTMLAudioElement>('audio'); }
+export function createBold() { return new Noliter('b'); }
