85b3b8a73f3d047382cfb123764be4b5ad499828
태재영 <tjy970721@gmail.com>
2020-10-18 20:19:51 +0900

    feat: area

--- src/Element.ts
+++ src/Element.ts
@@ -3,3 +3,4 @@ import Noliter from './Noliter';
 export function createAnchor() { return new Noliter<HTMLAnchorElement>('a'); }
 export function createAbbr() { return new Noliter('abbr'); }
 export function createAddress() { return new Noliter('address'); }
+export function createArea() { return new Noliter<HTMLAreaElement>('area'); }
