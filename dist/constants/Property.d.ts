9ee97ef515da900df52d0e60ed156c606394bb5a
태재영 <tjy970721@gmail.com>
2020-10-20 14:16:42 +0900

    0.3.1

--- dist/constants/Property.d.ts
+++ dist/constants/Property.d.ts
@@ -1,3 +1,6 @@
+export declare const PROP_ID = "id";
+export declare const PROP_TYPE = "type";
+export declare const PROP_CLASS = "className";
 export declare const PROP_VALUE = "value";
 export declare const PROP_CHECKED = "checked";
 export declare const PROP_DISABLED = "disabled";
--- src/constants/Property.ts
+++ src/constants/Property.ts
@@ -1,4 +1,6 @@
-// TODO id / className / type
+export const PROP_ID = 'id';
+export const PROP_TYPE = 'type';
+export const PROP_CLASS = 'className';
 export const PROP_VALUE = 'value';
 export const PROP_CHECKED = 'checked';
 export const PROP_DISABLED = 'disabled';
