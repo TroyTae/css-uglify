d70a1c1fbafd6f922ac3096d8f6cd06deb6511da
태재영 <tjy970721@gmail.com>
2020-10-22 10:15:36 +0900

    0.4.2

--- dist/Noliter.d.ts
+++ dist/Noliter.d.ts
@@ -3,7 +3,7 @@ declare type Primitive = boolean | number | string;
 declare class Noliter<K extends TagNames> {
     dom: HTMLElementTagNameMap[K];
     constructor(tagName: K);
-    append(...children: (string | Node | Noliter<K>)[]): this;
+    append(...children: (string | Node | Noliter<TagNames>)[]): this;
     attrs(...attributes: Primitive[]): this;
     props(...properties: Primitive[]): this;
     events<E extends keyof HTMLElementEventMap>(type: E, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): this;
