5b8f032162d68ab5fc5a4d3ab13281a6ddf99905
태재영 <tjy970721@gmail.com>
2020-10-23 14:00:59 +0900

    0.5.2

--- dist/constants/Attribute.d.ts
+++ dist/constants/Attribute.d.ts
@@ -3,3 +3,4 @@ export declare const ATTR_VALUE = "value";
 export declare const ATTR_CLASS = "class";
 export declare const ATTR_TYPE = "type";
 export declare const ATTR_STYLE = "style";
+export declare const ATTR_HREF = "href";
--- dist/constants/Property.d.ts
+++ dist/constants/Property.d.ts
@@ -4,3 +4,4 @@ export declare const PROP_CLASS = "className";
 export declare const PROP_VALUE = "value";
 export declare const PROP_CHECKED = "checked";
 export declare const PROP_DISABLED = "disabled";
+export declare const PROP_HREF = "href";
