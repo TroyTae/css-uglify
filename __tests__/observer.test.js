7e7d244133cbbeec88a109098b19a0932d2d437d
태재영 <tjy970721@gmail.com>
2021-04-17 22:37:19 +0900

    test: observer

---
+++ __tests__/observer.test.js
@@ -0,0 +1,36 @@
+const { createElement, createObserver } = require("../dist/index");
+
+test("subscribe value", () => {
+  const [getValue, setValue, subscribeValue] = createObserver(0);
+  subscribeValue((value) => {
+    expect(value).toBe(10);
+  });
+  expect(getValue()).toBe(0);
+  setValue(10);
+  expect(getValue()).toBe(10);
+});
+
+test("unsubscribe value", () => {
+  const span = createElement("span");
+  const [
+    getValue,
+    setValue,
+    subscribeValue,
+    unsubscribeValue,
+  ] = createObserver();
+
+  subscribeValue(() => {
+    span.textContent = "first";
+  });
+  const key = subscribeValue(() => {
+    span.textContent = "second";
+  });
+
+  setValue();
+  expect(span.textContent).toBe("second");
+  unsubscribeValue(key);
+
+  setValue();
+  expect(span.textContent).toBe("first");
+  expect(getValue()).toBe(undefined);
+});
