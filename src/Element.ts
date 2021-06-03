a09936112786f4f5f0552138bf86463d4a7f06ec
태재영 <tjy970721@gmail.com>
2021-06-03 23:57:41 +0900

    feat: createAddress

--- src/Element.ts
+++ src/Element.ts
@@ -21,6 +21,10 @@ export function createAbbr(builder: Builder<"abbr">) {
   return createElement("abbr", builder);
 }
 
+export function createAddress(builder: Builder<"address">) {
+  return createElement("address", builder);
+}
+
 export function removeChildren<N extends Node>(parent: N) {
   while (parent.lastChild) {
     parent.removeChild(parent.lastChild);
